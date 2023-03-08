adc006f4109d771adc775dfa89e45bcc5fc3931d
JymDigital8 <126175278+JymDigital8@users.noreply.github.com>
2023-03-08 13:41:20 -0400

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,8 +4,8 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
+      <Title>JYMDIGITAK8</Title>
+      <h1>JYMDIGITAL8</h1>
       <Counter />
       <p>
         Visit{" "}
